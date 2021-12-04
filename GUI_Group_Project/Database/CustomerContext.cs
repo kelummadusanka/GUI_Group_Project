5a8acca81b763bd31308abfce9d373824789b973
kelummadusanka <79236557+kelummadusanka@users.noreply.github.com>
2021-12-05 01:55:38 +0530

    add entityframword and dbcontext

---
+++ GUI_Group_Project/Database/CustomerContext.cs
@@ -0,0 +1,10 @@
+﻿using GUI_Group_Project.Models;
+using System.Data.Entity;
+
+namespace GUI_Group_Project.Database
+{
+    class CustomerContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
