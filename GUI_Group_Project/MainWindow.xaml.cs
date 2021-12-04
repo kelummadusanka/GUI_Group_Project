a853436985e92132c9ec5b77bc2ca130add38b6c
kelummadusanka <79236557+kelummadusanka@users.noreply.github.com>
2021-12-05 01:25:44 +0530

    add customer model

--- GUI_Group_Project/MainWindow.xaml.cs
+++ GUI_Group_Project/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +16,70 @@ using System.Windows.Shapes;
 
 namespace GUI_Group_Project
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void SignupBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //RegisterWindow registerWindow = new RegisterWindow();
+            //registerWindow.Show();
+            //this.Hide();
+
+        }
+
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e) => ShowPasswordFunction();
+        private void ToggleButton_UnChecked(object sender, RoutedEventArgs e) => HidePasswordFunction();
+
+        private void ShowPasswordFunction()
+        {
+            PasswordTxtBox.Visibility = Visibility.Visible;
+            LoginPasswordBox.Visibility = Visibility.Hidden;
+            PasswordTxtBox.Text = LoginPasswordBox.Password;
+        }
+        private void HidePasswordFunction()
+        {
+            PasswordTxtBox.Visibility = Visibility.Hidden;
+            LoginPasswordBox.Visibility = Visibility.Visible;
+            LoginPasswordBox.Password = PasswordTxtBox.Text;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoginButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            //using (CustomerContext context = new CustomerContext())
+            //{
+            //    var custmer = context.Customers.FirstOrDefault(c => c.Username == UsernameTextBox.Text || c.Email == UsernameTextBox.Text || c.ID.ToString() == UsernameTextBox.Text);
+            //    if (custmer != null)
+            //    {
+            //        if (custmer.Password == LoginPasswordBox.Password)
+            //        {
+            //            Application.Current.Properties["custmer"] = custmer;
+            //            MessageBox.Show("Login Successfull  " + custmer.FirstName);
+            //            Profile profile = new Profile();
+            //            profile.Show();
+            //            this.Close();
+            //        }
+
+            //        else
+            //        {
+            //            MessageBox.Show("Invalid Password!");
+            //        }
+            //    }
+
+            //    else
+            //    {
+            //        MessageBox.Show("Invalid Username or UserId!");
+            //    }
+            //}
         }
     }
 }
---
+++ GUI_Group_Project/Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_Group_Project.Models
+{
+    class Customer
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string IDCard { get; set; }
+        public string BirthDay { get; set; }
+        public string Address { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Zipcode { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
