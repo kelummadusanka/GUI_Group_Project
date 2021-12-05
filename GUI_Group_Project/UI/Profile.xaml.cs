f859f4aa687f27c75fe72de23ab295a341f0d0bf
kelummadusanka <79236557+kelummadusanka@users.noreply.github.com>
2021-12-05 11:19:09 +0530

    Add proile section

--- GUI_Group_Project/UI/Profile.xaml.cs
+++ GUI_Group_Project/UI/Profile.xaml.cs
@@ -1,23 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace GUI_Group_Project.UI
 {
     /// <summary>
     /// Interaction logic for Profile.xaml
     /// </summary>
-    public partial class Profile : Window
+    public partial class Profile : UserControl
     {
         public Profile()
         {
